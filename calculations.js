fc1b634211addacfdd7599b291f2444cd0cd5189
Anna Titova <amtitova@gmail.com>
2022-11-03 17:06:33 +0100

    6.4 complete

--- calculations.js
+++ calculations.js
@@ -26,7 +26,17 @@ function getRestAfterDivision(x, y) {
   return result;
 }
 
+function incrementValue(x) {
+  return ++x;
+}
+
+function decrementValue(x) {
+  return --x;
+}
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+console.log(incrementValue(234));
+console.log(decrementValue(456));
